a40de0a052ddaff169a2d23a94f56c5cd9fae1f1
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-12 19:40:09 -0400

    Switched to isinstance statements
    Signed-off-by: Taras Ivashchuk <taras@tarasivashchuk.com>

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,10 +1,9 @@
-from cards import *
+from portfolio import cards
 
 
 def test_get_projects_markdown():
-    projects = get_projects_markdown()
-    assert type(projects) == list
+    projects = cards.get_projects_markdown()
+    assert isinstance(projects, list)
     assert len(projects) > 0
     for project in projects:
-        assert type(project) == str
-
+        assert isinstance(project, str)
