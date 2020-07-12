a6a4b4d2dfe725d4985a2440056f0f89538c585a
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-12 13:32:33 -0400

    Created cards.py module
    - Added get_projects_markdown for loading content

---
+++ src/portfolio/cards.py
@@ -0,0 +1,10 @@
+from portfolio import project_dir
+
+projects_markdown_dir = project_dir.joinpath("references")
+
+
+def get_projects_markdown():
+    projects = []
+    for project_file in projects_markdown_dir.glob("*.md"):
+        projects.append(project_file.read_text())
+    return projects
