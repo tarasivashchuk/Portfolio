4c85df31f7ca98dca8536c92501f8aebcf0ecf2f
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-12 13:34:13 -0400

    Added contact form using flask_wtf and added app route

---
+++ src/portfolio/forms.py
@@ -0,0 +1,26 @@
+import flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+
+from src.portfolio.app import app
+
+
+class ContactForm(FlaskForm):
+    name = StringField("Name")
+    email = StringField("Email")
+    message = TextAreaField("Type your message here...")
+    submit = SubmitField(label="Send")
+
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    name = "Taras"
+    email = "taras@tarasivashchuk.com"
+    message = "Hello form!"
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        name, email, message = form.name.data, form.email.data, form.message.data
+        form.name.data, form.email.data, form.message.data = "", "", ""
+
+    return flask.render_template("contact.html", form=form, name=name, email=email, message=message)
