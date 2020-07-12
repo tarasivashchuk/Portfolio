851539a1ba6d9cd352ab046796f92d7155af6296
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-07-12 19:39:51 -0400

    Moved source files to portfolio dir and updated md path
    Signed-off-by: Taras Ivashchuk <taras@tarasivashchuk.com>

--- portfolio/cards.py
+++ portfolio/cards.py
@@ -1,6 +1,6 @@
 from . import project_dir
 
-projects_markdown_dir = project_dir.joinpath("references")
+projects_markdown_dir = project_dir.joinpath("static/md/projects")
 
 
 def get_projects_markdown():
@@ -8,3 +8,6 @@ def get_projects_markdown():
     for project_file in projects_markdown_dir.glob("*.md"):
         projects.append(project_file.read_text())
     return projects
+
+
+def parse_markdown(markdown: str):
